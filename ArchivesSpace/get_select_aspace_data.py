44ed81b6db29a88c49e610b3cba0d0ecc700b4df
Kayla Heslin <71670137+kheslin0420@users.noreply.github.com>
2022-06-22 07:18:36 -0400

    Update get_select_aspace_data.py

--- ArchivesSpace/get_select_aspace_data.py
+++ ArchivesSpace/get_select_aspace_data.py
@@ -12,13 +12,13 @@ import re
 from dateutil import parser
 from collections import Counter
 
-path = '/path/to/where/to/save/csv'
-baseURL = '' #use institutions baseURL for Aspace
-user_name = '' #aspace username
-password = '' #aspace api password
+path = '/Users/kaylaheslin/Desktop'
+baseURL = 'https://pittapi.as.atlas-sys.com'
+user_name = 'kmh250'
+password = 'hL9xE4rC8cI6'
 repo = input("Enter repo number:")
-record_type = 'archival_objects' #change according to record type 
-auth = requests.post('{}/users/{}/login?password={}'.format(baseURL, user_name, password)).json()
+record_type = 'archival_objects'
+auth = requests.post('https://pittapi.as.atlas-sys.com/users/{}/login?password={}'.format(user_name, password)).json()
 session = auth['session']
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
@@ -139,10 +139,10 @@ class archival_object:
 csv_path = input("Enter name for CSV:")
 csv_mode = input(
     'Are you opening and writing a new CSV or appending to an exisiting one?\nEnter w for new and a for appending:')
-heirarchy = input('Is their heirarchy to this collection? (ie are there series/subseries/record groups?) y/n?:').lower()
+heirarchy = input('Is their heirarchy to this collection? y/n?:').lower()
 
 ##Nested archival objects
-with open('{}/{}.csv'.format(path, csv_path), mode=csv_mode) as csv_file:
+with open('/Users/kaylaheslin/Desktop/{}.csv'.format(csv_path), mode=csv_mode) as csv_file:
     fieldnames = ['ref_id', 'ao_uri', 'title', 'identifier', 'normalized_date', 'normalized_date_qualifier',
                   'subject', 'subject_name', 'subject_geographic', 'description', 'type_of_resource', 'genre', 'creator', 'copyright_status',
                   'publication_status', 'source_collection_title', 'source_collection_id', 'source_series',
@@ -156,48 +156,108 @@ with open('{}/{}.csv'.format(path, csv_path), mode=csv_mode) as csv_file:
         arg = input(
             "AllseriesORsubseries, true or false? Enter true only if you are getting all items from a subseries within a series or multiple subseries nested under a series."
             "\nIf you are attempting to get items directly under a series, select false:")
+        #print(seriesORsubseries_num)
+        for level in seriesORsubseries_num.split(' '):
+            seriesORsuberies_level_endpoint = '/repositories/{}'.format(repo) + '/archival_objects/' + str(level)
+            #print(level)
 
-        seriesORsuberies_level_endpoint = '/repositories/{}'.format(repo) + '/archival_objects/' + str(
-            seriesORsubseries_num)
+            a = requests.get(baseURL + seriesORsuberies_level_endpoint, headers=headers).json()
+            # collection level metadata
 
-        a = requests.get(baseURL + seriesORsuberies_level_endpoint, headers=headers).json()
-        # collection level metadata
+            for ancestor in a['ancestors']:
+                if ancestor['level'] == 'collection':
+                    collection = requests.get(baseURL + ancestor['ref'], headers=headers).json()
+                    # print(json.dumps(collection, indent=2))
+                    source_collection_title = collection['title']
+                    source_collection_ID_list = []
+                    for item in collection:
+                        x = re.search('^id.\d$', item)
+                        if x != None:
+                            source_collection_ID_list.append(x.group())
+                            source_collection_ID = '.'.join([collection.get(item) for item in source_collection_ID_list])
+                    source_citation = collection['title'] + ', ' + collection['dates'][0][
+                        'expression'] + ', ' + source_collection_ID + ', ' + 'Archives & Special Collections, University of Pittsburgh'
 
-        for ancestor in a['ancestors']:
-            if ancestor['level'] == 'collection':
-                collection = requests.get(baseURL + ancestor['ref'], headers=headers).json()
-                # print(json.dumps(collection, indent=2))
-                source_collection_title = collection['title']
-                source_collection_ID_list = []
-                for item in collection:
-                    x = re.search('^id.\d$', item)
-                    if x != None:
-                        source_collection_ID_list.append(x.group())
-                        source_collection_ID = '.'.join([collection.get(item) for item in source_collection_ID_list])
-                source_citation = collection['title'] + ', ' + collection['dates'][0][
-                    'expression'] + ', ' + source_collection_ID + ', ' + 'Archives & Special Collections, University of Pittsburgh'
+            # object metadata
+            if arg == 'true':
+                children_endpoint = seriesORsuberies_level_endpoint + '/children'
+                ao = requests.get(baseURL + children_endpoint,
+                                  headers=headers).json()  # request the json for each item (subseries or children) under the series
+                for item in ao:  # looking at the items within the subseries or series
+                    # print(item['uri'])
+                    child_list = requests.get(baseURL + item['uri'] + '/children',
+                                              headers=headers).json()  # request the json for each item under the subseries
+                    for i in child_list:
+                        instance_type_list = []
+                        for instance in i['instances']:
+                            instance_type_list.append(instance['instance_type'])
+                        for count, value in enumerate(instance_type_list): #enumerate the instance list
+                            if value == 'digital_object': # we only want to get do instances
+                                do = requests.get(baseURL + i['instances'][count][value]['ref'], headers=headers).json() #then we get the DO
+                                #print(json.dumps(do, indent=2))
+                                ao_uri = do['linked_instances'][0]['ref'] #archival object for digital object
+                                identifier = do['digital_object_id']
+                                title = do['title']
+                                ao_1 = archival_object(uri=ao_uri, ref_id=i.get('ref_id'), notes=i.get('notes'),
+                                dates = i.get('dates'), ancestor = i.get('ancestors'), instances = i.get('instances'))
 
-        # object metadata
-        if arg == 'true':
-            children_endpoint = seriesORsuberies_level_endpoint + '/children'
-            ao = requests.get(baseURL + children_endpoint,
-                              headers=headers).json()  # request the json for each item (subseries or children) under the series
-            for item in ao:  # looking at the items within the subseries or series
-                # print(item['uri'])
-                child_list = requests.get(baseURL + item['uri'] + '/children',
-                                          headers=headers).json()  # request the json for each item under the subseries
-                for i in child_list:
+                                description = ao_1.get_description()
+                                normalized_date = ao_1.get_date()
+                                source_series = ao_1.get_series()
+                                source_subseries = ao_1.get_subseries()
+                                source_otherlevel = ao_1.get_otherLevel()
+                                source_container = ao_1.get_containers()
+                                ao_1.write_csv()
+                            else:
+                                continue
+                        if "digital_object" not in instance_type_list:
+                            title = i['title']
+                            identifier = ''
+                            ao_1 = archival_object(uri=i.get('uri'), ref_id=i.get('ref_id'), notes=i.get('notes'),
+                                                   dates=i.get('dates'), ancestor=i.get('ancestors'), instances=i.get('instances'))
+
+                            description = ao_1.get_description()
+                            normalized_date = ao_1.get_date()
+                            source_series = ao_1.get_series()
+                            source_subseries = ao_1.get_subseries()
+                            source_otherlevel = ao_1.get_otherLevel()
+                            source_container = ao_1.get_containers()
+                            ao_1.write_csv()
+                        else:
+                            continue
+
+            else:
+                children_endpoint = '/repositories/{}'.format(repo) + '/archival_objects/' + str(level) + '/children'
+                ao = requests.get(baseURL + children_endpoint,
+                                  headers=headers).json()  # request the json for each item (subseries or children) under the series
+                for item in ao:  # looking at the items within the subseries
                     instance_type_list = []
-                    for instance in i['instances']:
-                        instance_type_list.append(instance['instance_type'])
-                    if "digital_object" in instance_type_list:
-                        do = requests.get(
-                            baseURL + i['instances'][instance_type_list.index('digital_object')]['digital_object'][
-                                'ref'], headers=headers).json()
-                        identifier = do['digital_object_id']
-                        title = do['title']
-                        ao_1 = archival_object(uri=i.get('uri'), ref_id=i.get('ref_id'), notes=i.get('notes'),
-                                               dates=i.get('dates'), ancestor=i.get('ancestors'), instances=i.get('instances'))
+                    for i in item['instances']:
+                        instance_type_list.append(i['instance_type'])
+                    for count, value in enumerate(instance_type_list): #enumerate the instance list
+                        if value == 'digital_object': # we only want to get do instances
+                            do = requests.get(baseURL + item['instances'][count][value]['ref'], headers=headers).json() #then we get the DO
+                            #print(json.dumps(do, indent=2))
+                            ao_uri = do['linked_instances'][0]['ref'] #archival object for digital object
+                            identifier = do['digital_object_id']
+                            title = do['title']
+                            ao_1 = archival_object(uri=ao_uri, ref_id=item.get('ref_id'), notes=item.get('notes'),
+                            dates = item.get('dates'), ancestor = item.get('ancestors'), instances = item.get('instances'))
+
+                            description = ao_1.get_description()
+                            normalized_date = ao_1.get_date()
+                            source_series = ao_1.get_series()
+                            source_subseries = ao_1.get_subseries()
+                            source_otherlevel = ao_1.get_otherLevel()
+                            source_container = ao_1.get_containers()
+                            ao_1.write_csv()
+                        else:
+                            continue
+                    if "digital_object" not in instance_type_list:
+                        title = item['title']
+                        identifier = ''
+                        ao_1 = archival_object(uri=item.get('uri'), ref_id=item.get('ref_id'), notes=item.get('notes'),
+                                               dates=item.get('dates'), ancestor=item.get('ancestors'), instances=item.get('instances'))
 
                         description = ao_1.get_description()
                         normalized_date = ao_1.get_date()
@@ -206,57 +266,6 @@ with open('{}/{}.csv'.format(path, csv_path), mode=csv_mode) as csv_file:
                         source_otherlevel = ao_1.get_otherLevel()
                         source_container = ao_1.get_containers()
                         ao_1.write_csv()
-                    else:
-                        title = i['title']
-                        identifier = ''
-                        ao_1 = archival_object(uri=i.get('uri'), ref_id=i.get('ref_id'), notes=i.get('notes'),
-                                               dates=i.get('dates'), ancestor=i.get('ancestors'), instances=i.get('instances'))
-
-                        description = ao_1.get_description()
-                        normalized_date = ao_1.get_date()
-                        source_series = ao_1.get_series()
-                        source_subseries = ao_1.get_subseries()
-                        source_otherlevel = ao_1.get_otherLevel
-                        source_container = ao_1.get_containers()
-                        ao_1.write_csv()
-        else:
-            children_endpoint = '/repositories/{}'.format(repo) + '/archival_objects/' + str(
-                seriesORsubseries_num) + '/children'
-            ao = requests.get(baseURL + children_endpoint,
-                              headers=headers).json()  # request the json for each item (subseries or children) under the series
-            for item in ao:  # looking at the items within the subseries
-                instance_type_list = []
-                for i in item['instances']:
-                    instance_type_list.append(i['instance_type'])
-                if "digital_object" in instance_type_list:
-                    do = requests.get(
-                        baseURL + item['instances'][instance_type_list.index('digital_object')]['digital_object'][
-                            'ref'], headers=headers).json()
-                    identifier = do['digital_object_id']
-                    title = item['title']
-                    ao_1 = archival_object(uri=item.get('uri'), ref_id=item.get('ref_id'), notes=item.get('notes'),
-                                           dates=item.get('dates'), ancestor=item.get('ancestors'), instances=item.get('instances'))
-
-                    description = ao_1.get_description()
-                    normalized_date = ao_1.get_date()
-                    source_series = ao_1.get_series()
-                    source_subseries = ao_1.get_subseries()
-                    source_otherlevel = ao_1.get_otherLevel()
-                    source_container = ao_1.get_containers()
-                    ao_1.write_csv()
-                else:
-                    title = item['title']
-                    identifier = ''
-                    ao_1 = archival_object(uri=item.get('uri'), ref_id=item.get('ref_id'), notes=item.get('notes'),
-                                           dates=item.get('dates'), ancestor=item.get('ancestors'), instances=item.get('instances'))
-
-                    description = ao_1.get_description()
-                    normalized_date = ao_1.get_date()
-                    source_series = ao_1.get_series()
-                    source_subseries = ao_1.get_subseries()
-                    source_otherlevel = ao_1.get_otherLevel()
-                    source_container = ao_1.get_containers()
-                    ao_1.write_csv()
     else:
         collection_endpoint = input('Enter collection endpoint:')
         collection = requests.get(baseURL + collection_endpoint, headers=headers).json()
@@ -278,24 +287,26 @@ with open('{}/{}.csv'.format(path, csv_path), mode=csv_mode) as csv_file:
             instance_type_list = []
             for instance in ao['instances']:
                 instance_type_list.append(instance['instance_type'])
-            if "digital_object" in instance_type_list:
-                do = requests.get(
-                    baseURL + ao['instances'][instance_type_list.index('digital_object')]['digital_object']['ref'],
-                    headers=headers).json()
-                identifier = do['digital_object_id']
-                title = do['title']
-                ao_1 = archival_object(uri=ao.get('uri'), ref_id=ao.get('ref_id'), notes=ao.get('notes'),
-                                       dates=ao.get('dates'), ancestor=ao.get('ancestors'), instances=ao.get('instances'))
+            for count, value in enumerate(instance_type_list): #enumerate the instance list
+                if value == 'digital_object': # we only want to get do instances
+                    do = requests.get(baseURL + ao['instances'][count][value]['ref'], headers=headers).json() #then we get the DO
+                    #print(json.dumps(do, indent=2))
+                    ao_uri = do['linked_instances'][0]['ref'] #archival object for digital object
+                    identifier = do['digital_object_id']
+                    title = do['title']
+                    ao_1 = archival_object(uri=ao_uri, ref_id=ao.get('ref_id'), notes=ao.get('notes'),
+                    dates = ao.get('dates'), ancestor = ao.get('ancestors'), instances = ao.get('instances'))
 
-                description = ao_1.get_description()
-                normalized_date = ao_1.get_date()
-                source_series = ao_1.get_series()
-                source_subseries = ao_1.get_subseries()
-                source_otherlevel = ao_1.get_otherLevel()
-                source_container = ao_1.get_containers()
-                ao_1.write_csv()
-
-            else:
+                    description = ao_1.get_description()
+                    normalized_date = ao_1.get_date()
+                    source_series = ao_1.get_series()
+                    source_subseries = ao_1.get_subseries()
+                    source_otherlevel = ao_1.get_otherLevel()
+                    source_container = ao_1.get_containers()
+                    ao_1.write_csv()
+                else:
+                    continue
+            if "digital_object" not in instance_type_list:
                 title = ao['title']
                 identifier = ''
                 ao_1 = archival_object(uri=ao.get('uri'), ref_id=ao.get('ref_id'), notes=ao.get('notes'),
